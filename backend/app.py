e01f420fe0da33c7e636b2b3aa22a58874add287
ROVIR15 <ekoarieanto92@gmail.com>
2023-09-11 15:55:58 +0700

    adding new api's to app.py

--- backend/app.py
+++ backend/app.py
@@ -13,15 +13,35 @@ from Record.BCSRecord.views import views_bcs_record_bp
 from Record.WeightRecord.views import views_weight_record_bp
 from Record.HealthRecord.views import views_health_record_bp
 
+from FarmProfile.views import views_farm_profile_bp
 from Finance.Budget.views import views_budget_bp
 from Finance.BudgetItem.views import views_budget_item_bp
 from Finance.BudgetCategory.views import views_budget_category_bp
 
+# ----------------------------------------- #
+# Auth
+# ----------------------------------------- #
+from flask_login import LoginManager
+
+# Model
+from User.models import User as UserModel
+from User.views import views_auth_bp
+
 app = Flask(__name__)
+app.debug = False  # Turn off debug mode
+app.secret_key = "yLKXF7'EU5z3j~K1"  # Replace with a strong secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kucing%401@localhost:5432/'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return UserModel.query.get(int(user_id))
 
 # 1. Block Area register and management
 app.register_blueprint(views_block_area_bp)
@@ -56,6 +76,16 @@ app.register_blueprint(views_budget_bp)
 app.register_blueprint(views_budget_item_bp)
 # 12. Budget Category
 app.register_blueprint(views_budget_category_bp)
+
+
+# Auth
+# 0. Authentication
+app.register_blueprint(views_auth_bp)
+
+# Farm Profile
+# 13. Farm Profile API
+app.register_blueprint(views_farm_profile_bp)
+
 
 # app.register_blueprint(views_bp)
 # app.register_blueprint(views_sled_bp)
