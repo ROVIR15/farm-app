64a7a4e0917d308478b3786e0b50a0e479d42a4e
ROVIR15 <ekoarieanto92@gmail.com>
2023-09-11 15:55:21 +0700

    Breeding Feature API

---
+++ backend/Breeds/BreedingStatus/modul.py
@@ -0,0 +1,12 @@
+from db import db
+from datetime import datetime
+
+
+class BreedingStatus (db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    breeding_id = db.Column(
+        db.Integer(), db.ForeignKey('breeding.id'), nullable=False)
+    breeding_status_name_id = db.Column(
+        db.Integer(), nullable=False) #db.ForeignKey('breeding.id')
+    remarks = db.Column(db.Text(), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
---
+++ backend/Breeds/Pregnancy/modul.py
@@ -0,0 +1,11 @@
+from db import db
+from datetime import datetime
+
+
+class Pregnancy (db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    breeding_id = db.Column(
+        db.Integer(), db.ForeignKey('breeding.id'), nullable=False)
+    is_active = db.Column(
+        db.Bool(), nullable=False) #db.ForeignKey('breeding.id')
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
