c3ae3636d781d27a643bfbb4fb7fa879dd9398bc
ROVIR15 <ekoarieanto92@gmail.com>
2023-09-11 15:54:48 +0700

    add farm profile api feature

---
+++ backend/FarmProfile/models.py
@@ -0,0 +1,24 @@
+from db import db
+from datetime import datetime
+
+
+class FarmProfile (db.Model):
+    __tablename__ = "farm_profile"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), nullable=True)
+    address_one = db.Column(db.Text, nullable=True)
+    address_two = db.Column(db.String(256), nullable=True)
+    city = db.Column(db.String(100), nullable=True)
+    province = db.Column(db.String(100), nullable=True)
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+
+
+class FarmProfileHasUsers (db.Model):
+    farm_profile_id = db.Column(db.Integer(), db.ForeignKey(
+        'farm_profile.id'), primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey(
+        'user.id'), primary_key=True, nullable=False)
+
+    farm_profile = db.relationship('FarmProfile', back_populates='farm_profile_has_users', foreign_keys=[farm_profile_id], lazy=True)
+    users = db.relationship('User', back_populates='users', foreign_keys=[user_id], lazy=True)
---
+++ backend/FarmProfile/schema.py
@@ -0,0 +1,11 @@
+from marshmallow import Schema, fields
+from utils.index import CustomDateTimeField
+
+class FarmProfileSchema(Schema):
+    id = fields.Int(required=True)
+    name = fields.Str(required=True)
+    address_one = fields.Str(required=True)
+    address_two = fields.Str(required=True)
+    city = fields.Str(required=True)
+    province = fields.Str(required=True)
+    created_at = CustomDateTimeField()
