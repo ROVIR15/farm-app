61a71e8a98e4281282dea5aff424e39112b36c8b
Dwi Candra Kirana <dwicandra920@gmail.com>
2023-10-13 21:22:29 +0700

    fix ui record fattening

--- androidapp/Vt/app/src/main/java/com/vt/vt/ui/rekam_berat_badan/RekamBeratBadanFragment.kt
+++ androidapp/Vt/app/src/main/java/com/vt/vt/ui/rekam_berat_badan/RekamBeratBadanFragment.kt
@@ -77,21 +77,8 @@ class RekamBeratBadanFragment : Fragment() {
             getLivestockById.observe(viewLifecycleOwner) { livestock ->
                 with(binding) {
                     tvTitleLivestock.text = livestock?.name.toString()
-                    tvBangsaAnimal.text = livestock?.bangsa.toString()
+                    tvInfo.text = livestock?.info.toString()
                     tvDescriptionLivestock.text = livestock?.description.toString()
-                    when (livestock?.gender) {
-                        1 -> {
-                            tvDetailLivestockAnimalGender.text = "Jantan"
-                        }
-
-                        2 -> {
-                            tvDetailLivestockAnimalGender.text = "Betina"
-                        }
-
-                        else -> {
-                            tvDetailLivestockAnimalGender.text = "None"
-                        }
-                    }
                 }
             }
             isError().observe(viewLifecycleOwner) {
